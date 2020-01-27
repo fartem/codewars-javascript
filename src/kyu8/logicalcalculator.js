b5c909497c5e0064b1df06ce30ce200724bc55aa
Artem <jaman.smlnsk@gmail.com>
2020-01-27 22:12:19 +0300

    2020-01-27 Version 0.0.5: Updated "Logical calculator"

--- src/kyu8/logicalcalculator.js
+++ src/kyu8/logicalcalculator.js
@@ -2,30 +2,14 @@
  * https://www.codewars.com/kata/57096af70dad013aa200007b
  */
 
+const operations = {
+  'AND': (a, b) => a && b,
+  'OR': (a, b) => a || b,
+  'XOR': (a, b) => a !== b
+}
+
 function logicalCalculator (array, operation) {
-  switch (operation) {
-    case 'AND':
-      return and(array)
-    case 'OR':
-      return or(array)
-    case 'XOR':
-      return xor(array)
-  }
-}
-
-function and (array) {
-  if (array.every((x) => x === true) || array.every((x) => x === false)) {
-    return array[0]
-  }
-  return false
-}
-
-function or (array) {
-  return array.reduce((x, y, z) => { return x === y ? x : true })
-}
-
-function xor (array) {
-  return array.reduce((x, y, z) => x !== y)
+  return array.reduce(operations[operation])
 }
 
 module.exports = {
